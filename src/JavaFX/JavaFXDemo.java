d03c104a9fbb57fd5aee242711e035aa4aa01179
anupchand007 <2078bit@gmail.com>
2025-06-05 20:40:21 +0545

    Updates

---
+++ src/JavaFX/JavaFXDemo.java
@@ -0,0 +1,31 @@
+package JavaFX;
+import javafx.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFXDemo extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+        Button btn = new Button("Click me Bitch");
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
